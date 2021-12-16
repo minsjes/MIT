13b8fda94c1c41a03484c8653b897efd5f4a34ab
이채연 <Lee@DESKTOP-D7O864G>
2021-12-17 01:16:58 +0900

    질문, 자유 게시판 맵퍼랑 VO 수정

--- MIT/src/main/java/com/mit/domain/FreeCommentVO.java
+++ MIT/src/main/java/com/mit/domain/FreeCommentVO.java
@@ -1,20 +1,31 @@
 package com.mit.domain;
 
-import java.util.Date;
-
 public class FreeCommentVO {
 	
 	private int commentNo;
+	private String commentText;
+	private String commentDate;
 	private int freeNo;
 	private int memberNo;
 	private String memberName;
-	private String commentText;
-	private Date commentDate;
+	
 	public int getCommentNo() {
 		return commentNo;
 	}
 	public void setCommentNo(int commentNo) {
 		this.commentNo = commentNo;
+	}
+	public String getCommentText() {
+		return commentText;
+	}
+	public void setCommentText(String commentText) {
+		this.commentText = commentText;
+	}
+	public String getCommentDate() {
+		return commentDate;
+	}
+	public void setCommentDate(String commentDate) {
+		this.commentDate = commentDate;
 	}
 	public int getFreeNo() {
 		return freeNo;
@@ -34,24 +45,4 @@ public class FreeCommentVO {
 	public void setMemberName(String memberName) {
 		this.memberName = memberName;
 	}
-	public String getCommentText() {
-		return commentText;
-	}
-	public void setCommentText(String commentText) {
-		this.commentText = commentText;
-	}
-	public Date getCommentDate() {
-		return commentDate;
-	}
-	public void setCommentDate(Date commentDate) {
-		this.commentDate = commentDate;
-	}
-	@Override
-	public String toString() {
-		return "FreeCommentVO [commentNo=" + commentNo + ", freeNo=" + freeNo + ", memberNo=" + memberNo
-				+ ", memberName=" + memberName + ", commentText=" + commentText + ", commentDate=" + commentDate + "]";
-	}
-		
-	
-	
 }
--- MIT/src/main/java/com/mit/domain/QnaCommentVO.java
+++ MIT/src/main/java/com/mit/domain/QnaCommentVO.java
@@ -1,17 +1,31 @@
 package com.mit.domain;
 
-import java.util.Date;
 
 public class QnaCommentVO {
-
 	private int commentNo;
+	private String commentText;
+	private String commentDate;
 	private int qnaNo;
 	private int memberNo;
 	private String memberName;
-	private String commentText;
-	private Date commentDate;
+	
 	public int getCommentNo() {
 		return commentNo;
+	}
+	public void setCommentNo(int commentNo) {
+		this.commentNo = commentNo;
+	}
+	public String getCommentText() {
+		return commentText;
+	}
+	public void setCommentText(String commentText) {
+		this.commentText = commentText;
+	}
+	public String getCommentDate() {
+		return commentDate;
+	}
+	public void setCommentDate(String commentDate) {
+		this.commentDate = commentDate;
 	}
 	public int getQnaNo() {
 		return qnaNo;
@@ -31,28 +45,10 @@ public class QnaCommentVO {
 	public void setMemberName(String memberName) {
 		this.memberName = memberName;
 	}
-	public String getCommentText() {
-		return commentText;
-	}
-	public void setCommentText(String commentText) {
-		this.commentText = commentText;
-	}
-	public Date getCommentDate() {
-		return commentDate;
-	}
-	public void setCommentDate(Date commentDate) {
-		this.commentDate = commentDate;
-	}
-	public void setCommentNo(int commentNo) {
-		this.commentNo = commentNo;
-	}
+	
 	@Override
 	public String toString() {
-		return "QnaCommentVO [commentNo=" + commentNo + ", qnaNo=" + qnaNo + ", memberNo=" + memberNo + ", memberName="
-				+ memberName + ", commentText=" + commentText + ", commentDate=" + commentDate + "]";
+		return "QnaCommentVO [commentNo=" + commentNo + ", commentText=" + commentText + ", commentDate=" + commentDate
+				+ ", qnaNo=" + qnaNo + ", memberNo=" + memberNo + ", memberName=" + memberName + "]";
 	}
-	
-	
-	
 }
-	
